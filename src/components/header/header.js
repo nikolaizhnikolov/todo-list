ac0e0ccd37d0d75cb5bf4438716f91d41878128e
Nikolay Nikolov <nikolaizhnikolov@gmail.com>
2023-01-15 15:30:36 +0200

    Update header with logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,17 @@
 import headerStyle from "./header.css";
+import logo from "./list.png";
 
 export const header = function () {
     const header = document.createElement("header");
     header.classList.add("header");
-    header.textContent = "To-Do be do bee do...";
+
+    const img = document.createElement("img");
+    img.src = logo;
+
+    const text = document.createElement("div");
+    text.textContent = "To-Do be do bee do...";
+
+    header.appendChild(img);
+    header.appendChild(text);
     return header;
 };

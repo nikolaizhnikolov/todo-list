5a33ff284476cc10c844403c89c06cc9ca870dca
Nikolay Nikolov <nikolaizhnikolov@gmail.com>
2023-01-15 14:01:36 +0200

    Update layout. Add font

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import { footer } from "./components/footer/footer";
+import cssReset from "./reset.css";
 import css from "./style.css";
 
-function layout() {
-    const div = () => document.createElement("div");
+(function layout() {
+    const divWithClass = (c) => {
+        const div = document.createElement("div");
+        div.classList.add(c);
+        return div;
+    };
 
     const header = document.createElement("header");
     header.textContent = "To-Do Be Do Bee Do";
     document.body.appendChild(header);
 
-    const content = div();
-    content.classList.add("content");
+    const content = divWithClass("content");
+    content.appendChild(divWithClass("projects"));
+    content.appendChild(divWithClass("todos"));
     document.body.appendChild(content);
 
     document.body.appendChild(footer());
-}
-
-layout();
+})();

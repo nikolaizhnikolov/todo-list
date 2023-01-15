9f4312113c2e6ce0c59d8aed46c548c3097f6e5e
Nikolay Nikolov <nikolaizhnikolov@gmail.com>
2023-01-15 12:44:53 +0200

    Add dev server for live reload

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,3 @@
 import css from "./style.css";
 
-console.log("working");
+console.log("working reloaded");
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "todo.bundle.js",
     },
+    devServer: {
+        static: "./dist",
+    },
     module: {
         rules: [{ test: /\.css$/i, use: ["style-loader", "css-loader"] }],
     },

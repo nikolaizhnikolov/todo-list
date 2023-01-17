c7f4805380a6b2bffe83e9678d4017245de82732
Nikolay Nikolov <nikolaizhnikolov@gmail.com>
2023-01-17 17:00:18 +0200

    Add base styles for todo components

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,8 +1,30 @@
 import { todos } from "../../storage";
+import style from "./todo.css";
 
 export const todoComponent = function(todo) {
     const t = document.createElement("div");
-    t.textContent = todo.name;
+    t.classList.add("todo");
+
+    const checkbox = document.createElement("img");
+    checkbox.classList.add("todo__checkbox");
+    checkbox.classList.add("logo");
+
+    const name = document.createElement("div");
+    name.classList.add("todo__name");
+    name.textContent = todo.name;
+
+    const priority = document.createElement("div");
+    priority.classList.add("todo__priority");
+    priority.textContent = todo.priority;
+
+    const date = document.createElement("date");
+    date.classList.add("todo__date")
+    date.textContent = todo.date;
+    
+    t.appendChild(checkbox);
+    t.appendChild(name);
+    t.appendChild(priority);
+    t.appendChild(date);
     return t;
 }
 
